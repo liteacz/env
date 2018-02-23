86ec63d9ef0a9e9a62b4e8a8b82b0f57b80d3971
Michal Odcházel <michal.odchazel@litea.cz>
2018-02-23 17:06:58 +0100

    - initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Litea Env helper
+
+This class is very simply for getting env variables
+
+## How to install
+
+In `composer.json`:
+```json
+"repositories": [
+    {
+      "type": "vcs",
+      "url": "ssh://git@gitlab.litea.cz:2222/litea/env.git"
+    }
+  ]
+```
+
+and `composer reqired litea/env`
---
+++ src/Env.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace Litea\Utils;
+
+/**
+ * Class Env
+ *
+ * @package Litea\Utils
+ */
+class Env {
+
+	/**
+	 * @param string $name
+	 * @param null   $default
+	 *
+	 * @return bool|int|null|string
+	 */
+	public static function get(string $name, $default = null) {
+		$value = getenv($name);
+		if ($value === false) {
+			return $default;
+		}
+
+		return self::convert($value);
+	}
+
+	/**
+	 * @param string $name
+	 * @param string $delimiter
+	 * @param null   $default
+	 *
+	 * @return array
+	 */
+	public static function getArray(string $name, string $delimiter = ';', $default = null) {
+		$value = self::get($name . $default);
+
+		return explode($delimiter, $value);
+	}
+
+	/**
+	 * @param $value
+	 *
+	 * @return bool|int|null|string
+	 */
+	protected static function convert($value) {
+		$value = strtolower($value);
+
+		switch ($value) {
+			case 'true':
+				return true;
+
+			case 'false':
+				return false;
+
+			case 'null':
+				return null;
+		}
+
+		if (ctype_digit($value)) {
+			return (int)$value;
+		}
+
+		return $value;
+	}
+
+}
