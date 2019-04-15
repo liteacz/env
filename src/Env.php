6a0d9ef5c65767a64faae9229aa301f9c22fea44
Jan Neterda <jan.neterda@litea.cz>
2019-04-15 11:12:02 +0200

    Remove php 7.0 testing due its - visibility of class constants was only added in PHP 7.1

--- src/Env.php
+++ src/Env.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types=1);
+
+declare(strict_types = 1);
 
 namespace Litea\Utils;
 
@@ -8,75 +9,88 @@ namespace Litea\Utils;
  *
  * @package Litea\Utils
  */
-class Env {
+class Env
+{
 
-	const DELIMITER = ';';
+    const DELIMITER = ';';
 
-	/**
-	 * @param string $name
-	 * @param null   $default
-	 *
-	 * @return bool|int|null|string
-	 */
-	public static function get(string $name, $default = null) {
-		$value = getenv($name);
-		if ($value === false) {
-			return self::convert($default);
-		}
+    /**
+     * @param string $name
+     * @param null   $default
+     *
+     * @return bool|int|null|string
+     */
+    public static function get(string $name, $default = null)
+    {
 
-		return self::convert($value);
-	}
+        $value = getenv($name);
+        if ($value === false) {
+            return self::convert($default);
+        }
 
-	/**
-	 * @param string $name
-	 * @param null   $default
-	 * @param string $delimiter
-	 *
-	 * @return array
-	 */
-	public static function getArray(string $name, $default = null, string $delimiter = self::DELIMITER): array {
-		$value = self::get($name, $default);
+        return self::convert($value);
+    }
 
-		if (is_string($value)) {
-			return explode($delimiter, $value);
-		}
+    /**
+     * @param string $name
+     * @param null   $default
+     * @param string $delimiter
+     *
+     * @return array
+     */
+    public static function getArray(string $name, $default = null, string $delimiter = self::DELIMITER): array
+    {
 
-		return [];
-	}
+        $value = self::get($name, $default);
 
-	/**
-	 * @param $value
-	 *
-	 * @return bool|int|null|string
-	 */
-	protected static function convert($value) {
-		if ($value === null) {
-			return $value;
-		} else if (is_bool($value)) {
-			return $value;
-		} else if (preg_match('/^[0]+[0-9]+$/', (string)$value)) {
-			return (string)$value;
-		} else if (preg_match('/^[0-9]+$/', (string)$value)) {
-			return (int)$value;
-		}
+        if (is_string($value)) {
+            return explode($delimiter, $value);
+        }
 
-        if(is_array($value)){
+        return [];
+    }
+
+    /**
+     * @param $value
+     *
+     * @return bool|int|null|string
+     */
+    protected static function convert($value)
+    {
+
+        if ($value === null) {
+            return $value;
+        } else {
+            if (is_bool($value)) {
+                return $value;
+            } else {
+                if (preg_match('/^[0]+[0-9]+$/', (string)$value)) {
+                    return (string)$value;
+                } else {
+                    if (preg_match('/^[0-9]+$/', (string)$value)) {
+                        return (int)$value;
+                    }
+                }
+            }
+        }
+
+        if (is_array($value)) {
             return null;
         }
 
-		switch (strtolower($value)) {
-			case 'true':
-				return true;
+        switch (strtolower($value)) {
+            case 'true':
+                return true;
 
-			case 'false':
-				return false;
+            case 'false':
+                return false;
 
-			case 'null':
-				return null;
-		}
+            case 'null':
+                return null;
+        }
 
 
-		return $value;
-	}
+        return $value;
+    }
 
 }
